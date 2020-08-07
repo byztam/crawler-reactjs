d53a13bd75c9344ee349e2a588bfcf1a497ed865
tmtam3298 <byztam3298@gmail.com>
2020-08-08 00:08:28 +0700

    Updates

---
+++ precache-manifest.c9b6ae886fce6c4ec1b23b0ca8942578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb23306231c81d948d528acac324cdfc",
+    "url": "/crawler-reactjs/index.html"
+  },
+  {
+    "revision": "8ae6d6a0b5fbd35f7bb7",
+    "url": "/crawler-reactjs/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "21ef22a7cd8844507954",
+    "url": "/crawler-reactjs/static/css/main.559cd7fd.chunk.css"
+  },
+  {
+    "revision": "8ae6d6a0b5fbd35f7bb7",
+    "url": "/crawler-reactjs/static/js/2.58c26c8c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/crawler-reactjs/static/js/2.58c26c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ef22a7cd8844507954",
+    "url": "/crawler-reactjs/static/js/main.fa7329fa.chunk.js"
+  },
+  {
+    "revision": "6ac0b051ce772f93ba2f",
+    "url": "/crawler-reactjs/static/js/runtime-main.37ec8c2f.js"
+  }
+]);
