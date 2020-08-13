35f81a5f497de0cf6dd2062ec59636c1d88ef9cd
tmtam <byztam3298@gmail.com>
2020-08-14 00:28:59 +0700

    commit source

--- src/components/home/contents/BodyContentFirst.js
+++ src/components/home/contents/BodyContentFirst.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Common.css';
 import '../css/BodyContentFirst.css';
 import {
@@ -7,169 +7,86 @@ import {
     Button,
     Container
 } from 'reactstrap';
-function BodyContentFirst(){
-    return (
-        <Container className="p-0 mt-5">
+class BodyContentFirst extends Component {
+    render() {
+        var items = [
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            },
+            {
+                img: "https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg",
+                name: "Oklahoma",
+                category: "WordPress Theme"
+            }
+        ];
+
+        var elements = items.map((item, index) => {
+            return (
+                <Col xs="12" sm="12" lg="3" key={index}>
+                    <Col xs="12" className="product p-0">
+                        <Col xs="12" className="product-logo p-0">
+                            <img src={item.img} alt="" />
+                        </Col>
+                        <Col xs="12" className="product-detail">
+                            <Row className="m-0">
+                                <Col xs="10" className="pr-0">
+                                    <h6 className="product-name">{item.name}</h6>
+                                    <h6 className="product-category">{item.category}</h6>
+                                </Col>
+                                <Col xs="2" className="pl-0">
+                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Col>
+                </Col>
+            );
+        });
+        return (
+            <Container className="p-0 mt-5">
             <Row>
                 <Col xs="12" sm="12" lg="6" className="mt-5">
                     <strong><h5>SUPPORT OUR PRODUCT</h5></strong>
                 </Col>
             </Row>
             <Row>
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
-
-                <Col xs="12" sm="12" lg="3">
-                    <Col xs="12" className="product p-0">
-                        <Col xs="12" className="product-logo p-0">
-                            <img src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/theme-block1.jpg" alt="" />
-                        </Col>
-                        <Col xs="12" className="product-detail">
-                            <Row className="m-0">
-                                <Col xs="10" className="pr-0">
-                                    <h6 className="product-name">Oklahoma</h6>
-                                    <h6 className="product-category">WordPress Theme</h6>
-                                </Col>
-                                <Col xs="2" className="pl-0">
-                                    <Button className="product-btn-search"><i className="fa fa-search"></i></Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Col>
-                </Col>
+                {elements}
             </Row>
         </Container>
-    );
+        );
+    }
 }
 
 export default BodyContentFirst;
--- src/components/home/contents/HeaderContent.js
+++ src/components/home/contents/HeaderContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/HeaderContent.css';
 import '../css/Common.css';
 import {
@@ -11,72 +11,69 @@ import {
     Input,
     Container
 } from 'reactstrap';
-function HeaderContent(){
-    return (
-        <Container className="p-0">
-            <Row>
-                <Col xs="12" sm="12" lg="6" className="mt-5">
-                    <h6>KNOWLEDGE BASE</h6>
-                    <h3>We offer premium support and efficient solutions</h3>
-                    <InputGroup className="mt-5">
-                        <Input className="txt-search border-right-0" placeholder="Search" />
-                        <InputGroupAddon addonType="append" className="search-append">
-                            <InputGroupText className="btn0-search bg-transparent white">
-                                <Button className="btn-search"><i className="fa fa-search"></i></Button>
-                            </InputGroupText>
-                        </InputGroupAddon>
-                    </InputGroup>
-                </Col>
-                <Col xs="12" sm="12" lg="6" >
-                    <img className="img-content-1-2" src="https://byztam.github.io/crawler-reactjs/images/support-illustration-600x698.png" alt=""/>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs="12" sm="12" lg="4">
+class HeaderContent extends Component {
+    render() {
+        var items = [
+            {
+                faIcon: "fal fa-cog card-header-icon",
+                title: "Help and Documentation",
+                content: "Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites"
+            },
+            {
+                faIcon: "fal fa-comment-alt-lines card-header-icon",
+                title: "Community and Support",
+                content: "Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites"
+            },
+            {
+                faIcon: "fal fa-laptop-code card-header-icon",
+                title: "Developer and Resources",
+                content: "Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites"
+            }
+        ];
+
+        var elements = items.map((item, index) => {
+            return (
+                <Col xs="12" sm="12" lg="4" key={index}>
                     <Col xs="12" className="card-header">
                         <div style={{width: '100%', textAlign: 'center'}}>
-                            <i className="fal fa-cog card-header-icon"></i>
+                            <i className={item.faIcon}></i>
                         </div>
-                        <h4 className="card-header-title">Help and Documentation</h4>
+                            <h4 className="card-header-title">{item.title}</h4>
                         <div>
-                            <p className="card-header-content">Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites</p>
+                            <p className="card-header-content">{item.content}</p>
                         </div>
                         <div className="card-header-footer">
                             <a href="#top">Read More <i className="fa fa-angle-double-right"></i></a>
                         </div>
                     </Col>
                 </Col>
-                <Col xs="12" sm="12" lg="4">
-                    <Col xs="12" className="card-header">
-                        <div style={{width: '100%', textAlign: 'center'}}>
-                            <i className="fal fa-comment-alt-lines card-header-icon"></i>
-                        </div>
-                        <h4 className="card-header-title">Community and Support</h4>
-                        <div>
-                            <p className="card-header-content">Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites</p>
-                        </div>
-                        <div className="card-header-footer">
-                            <a href="#top">Read More <i className="fa fa-angle-double-right"></i></a>
-                        </div>
+            );
+        });
+        return (
+            <Container className="p-0">
+                <Row>
+                    <Col xs="12" sm="12" lg="6" className="mt-5">
+                        <h6>KNOWLEDGE BASE</h6>
+                        <h3>We offer premium support and efficient solutions</h3>
+                        <InputGroup className="mt-5">
+                            <Input className="txt-search border-right-0" placeholder="Search" />
+                            <InputGroupAddon addonType="append" className="search-append">
+                                <InputGroupText className="btn0-search bg-transparent white">
+                                    <Button className="btn-search"><i className="fa fa-search"></i></Button>
+                                </InputGroupText>
+                            </InputGroupAddon>
+                        </InputGroup>
                     </Col>
-                </Col>
-                <Col xs="12" sm="12" lg="4">
-                    <Col xs="12" className="card-header">
-                        <div style={{width: '100%', textAlign: 'center'}}>
-                            <i className="fal fa-laptop-code card-header-icon"></i>
-                        </div>
-                        <h4 className="card-header-title">Developer and Resources</h4>
-                        <div>
-                            <p className="card-header-content">Unlike other Frameworks which try to cover everything, It has been built specifically for WordPress and blog websites</p>
-                        </div>
-                        <div className="card-header-footer">
-                            <a href="#top">Read More <i className="fa fa-angle-double-right"></i></a>
-                        </div>
+                    <Col xs="12" sm="12" lg="6" >
+                        <img className="img-content-1-2" src="https://byztam.github.io/crawler-reactjs/images/support-illustration-600x698.png" alt=""/>
                     </Col>
-                </Col>
-            </Row>
-        </Container>
-    );
+                </Row>
+                <Row>
+                    {elements}
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default HeaderContent;
