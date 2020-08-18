cadeb7736f1a35246e19f4c4fbc29cff0fa2ee58
tmtam <byztam3298@gmail.com>
2020-08-19 00:03:35 +0700

    Updates

---
+++ precache-manifest.335374463c1a61cedbeb33834ad591c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba033fda713c03734b0c659901c473bb",
+    "url": "/crawler-reactjs/index.html"
+  },
+  {
+    "revision": "d053f7131c1875e331e2",
+    "url": "/crawler-reactjs/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b50f58ce2cca9987cf99",
+    "url": "/crawler-reactjs/static/css/main.aed589c0.chunk.css"
+  },
+  {
+    "revision": "d053f7131c1875e331e2",
+    "url": "/crawler-reactjs/static/js/2.0d433e20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crawler-reactjs/static/js/2.0d433e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50f58ce2cca9987cf99",
+    "url": "/crawler-reactjs/static/js/main.61710ce7.chunk.js"
+  },
+  {
+    "revision": "6ac0b051ce772f93ba2f",
+    "url": "/crawler-reactjs/static/js/runtime-main.37ec8c2f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crawler-reactjs/precache-manifest.2b3be2877502bbef0e817eb053d97942.js"
+  "/crawler-reactjs/precache-manifest.335374463c1a61cedbeb33834ad591c2.js"
 );
 
 self.addEventListener('message', (event) => {
