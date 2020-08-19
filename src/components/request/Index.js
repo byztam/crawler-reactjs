03e3cbaad158d5acc659f90800888b8f6398f2cc
tmtam <byztam3298@gmail.com>
2020-08-19 23:05:46 +0700

    commit source

--- src/components/request/Index.js
+++ src/components/request/Index.js
@@ -3,7 +3,10 @@ import Breadcrumbs from './../common/Breadcrumb';
 import {
     Row,
     Col,
-    Input
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText
 } from 'reactstrap';
 import './style.css';
 
@@ -31,11 +34,51 @@ class Request extends Component {
                             <Col xs="12" sm="12" md="6" lg="6" className="content-1">
                                 <img className="img" src="https://wikb.modeltheme.com/wp-content/uploads/2019/06/contact-illustration-600x644.png" alt="" />
                             </Col>
-                            <Col xs="12" sm="12" md="6" lg="6">
-                                <Input />
-                                <Input />
-                                <Input />
-                                <Input />
+                            <Col xs="12" sm="12" md="6" lg="6" className="content-2">
+                                <InputGroup className="mt-3">
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText className="left-icon bg-transparent">
+                                        <i className="fal fa-user"></i>
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder="Full name" />
+                                </InputGroup>
+
+                                <InputGroup className="mt-3">
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText className="left-icon bg-transparent">
+                                        <i className="fal fa-address-card"></i>
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder="Department" />
+                                </InputGroup>
+
+                                <InputGroup className="mt-3">
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText className="left-icon bg-transparent">
+                                        <i className="fal fa-building"></i>
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder="Building Number" />
+                                </InputGroup>
+
+                                <InputGroup className="mt-3">
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText className="left-icon bg-transparent">
+                                        <i className="fal fa-at"></i>
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder="Email" />
+                                </InputGroup>
+
+                                <InputGroup className="mt-3">
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText className="left-icon-textarea bg-transparent">
+                                        <i className="fal fa-edit icon-textarea"></i>
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input type="textarea" placeholder="Comment and Questions" />
+                                </InputGroup>
                             </Col>
                         </Row>
                     </Col>
